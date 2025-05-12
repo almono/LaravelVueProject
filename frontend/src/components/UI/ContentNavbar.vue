9a7beddff450ba091a016a0a251962b03c305b9c
Paweł Walczykiewicz <pwalczykiewicz93@gmail.com>
2025-05-12 15:35:52 +0200

    Navbar tweaks

--- frontend/src/components/UI/ContentNavbar.vue
+++ frontend/src/components/UI/ContentNavbar.vue
@@ -1,20 +1,28 @@
 <template>
   <div>
-    <BNavbar class="px-4 text-light" toggleable="lg" variant="light" pills>
+    <BNavbar class="px-4 py-3 text-light" toggleable="lg" variant="light">
       <BNavbarBrand href="#">Dashboard</BNavbarBrand>
       <BNavbarToggle target="nav-collapse" />
       <BCollapse id="nav-collapse" is-nav>
         <BNavbarNav class="ms-auto mb-2 mb-lg-0">
-          <BNavItemDropdown text="Lang" right>
-            <BDropdownItem href="#">EN</BDropdownItem>
-            <BDropdownItem href="#">ES</BDropdownItem>
-            <BDropdownItem href="#">RU</BDropdownItem>
-            <BDropdownItem href="#">FA</BDropdownItem>
-          </BNavItemDropdown>
-          <BNavItemDropdown right>
+          <BNavItemDropdown class="navbar-dropdown" text="Lang" right>
             <template #button-content>
               <span class="material-icons">
-                account_circle
+                language
+              </span>
+            </template>
+          </BNavItemDropdown>
+          <BNavItemDropdown class="navbar-dropdown" text="Notifications" right>
+            <template #button-content>
+              <span class="material-icons">
+                notifications
+              </span>
+            </template>
+          </BNavItemDropdown>
+          <BNavItemDropdown class="navbar-dropdown" right>
+            <template #button-content>
+              <span class="material-icons">
+                person
               </span>
             </template>
             <BDropdownItem href="#">Profile</BDropdownItem>
@@ -37,8 +45,21 @@ export default {
 };
 </script>
 
-<style lang="css" scoped>
-.dropdown-toggle::after {
+<style lang="css">
+.navbar-dropdown {
+  border-radius: 0px !important;
+  display: flex !important;
+  align-items: center !important;
+}
+.navbar-dropdown > div > button {
+  display: flex !important;
+  padding: 0px 10px 0px 10px !important;
+}
+.navbar-dropdown > div > ul {
+  border-radius: 0px !important;
+  top: 5px !important;
+}
+.navbar-dropdown > div > button::after {
   display: none !important;
 }
 </style>
